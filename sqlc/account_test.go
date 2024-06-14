2101c0f2afc6c083fef0121b62945a5a610b63d7
dunky-star <dunkygeoffrey39@gmail.com>
2024-06-13 22:42:56 -0500

    Writing unit  Main and CreateAccount unit tests

--- sqlc/account_test.go
+++ sqlc/account_test.go
@@ -4,13 +4,14 @@ import (
 	"context"
 	"testing"
 
+	"github.com/dunky-star/u_bank/util"
 	"github.com/stretchr/testify/require"
 )
-func TestCreateAccount(t *testing.T){
+func createRandomAccount(t *testing.T) Account{
 	arg := CreateAccountParams{
-		Owner: "Opiyo",
-		Balance: 1000,
-		Currency: "USD",
+		Owner: util.RandomOwner(),
+		Balance: util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
@@ -24,4 +25,10 @@ func TestCreateAccount(t *testing.T){
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
 
+	return account
+
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
 }
