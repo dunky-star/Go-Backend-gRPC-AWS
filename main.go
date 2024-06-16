ca6cd0514504e276ab8f0ca0ba7b211a817042fd
dunky-star <dunkygeoffrey39@gmail.com>
2024-06-16 14:36:16 -0500

    Transaction handling in Go

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
 
-import (
-	"database/sql"
-	"log"
+// import (
+// 	"database/sql"
+// 	"log"
 
-	_ "github.com/lib/pq"
-)
+// 	_ "github.com/lib/pq"
+// )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://postgres:cluster@1@localhost:5432/u_bank?sslmode=disable"
-	serverAddress = "0.0.0.:9090"
-)
+// const (
+// 	dbDriver      = "postgres"
+// 	dbSource      = "postgresql://postgres:cluster@1@localhost:5432/u_bank?sslmode=disable"
+// 	serverAddress = "0.0.0.:9090"
+// )
 
-func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
-	if err != nil{
-		log.Fatal("Cannot connect to DB: ", err)
-	}
+// func main() {
+// 	conn, err := sql.Open(dbDriver, dbSource)
+// 	if err != nil{
+// 		log.Fatal("Cannot connect to DB: ", err)
+// 	}
 
-	store := db.NewStore(conn)
-}
+// 	store := db.NewStore(conn)
+// }
