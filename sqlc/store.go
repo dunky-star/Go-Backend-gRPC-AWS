6ff297737446b802b3203afd7e4035a2665b7a6c
dunky-star <dunkygeoffrey39@gmail.com>
2024-06-16 15:59:08 -0500

    Transaction handling in Go

--- sqlc/store.go
+++ sqlc/store.go
@@ -91,24 +91,43 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
             return err
         }
 		// Update account's balance
+       if arg.FromAccountID < arg.ToAccountID {
+            result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+                ID:      arg.FromAccountID,
+                Amount:  -arg.Amount,
+            })
+            if err != nil {
+                return err
+            }
 
-        result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-            ID:      arg.FromAccountID,
-            Amount:  -arg.Amount,
-        })
-        if err != nil {
-            return err
-        }
 
+            result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+                ID:      arg.ToAccountID,
+                Amount: arg.Amount,
+            })
+            if err != nil {
+                return err
+            }
 
-        result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-            ID:      arg.ToAccountID,
-            Amount: arg.Amount,
-        })
-        if err != nil {
-            return err
-        }
+       } else {
+            result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+                ID:      arg.ToAccountID,
+                Amount: arg.Amount,
+            })
+            if err != nil {
+                return err
+            }
 
+            result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+                ID:      arg.FromAccountID,
+                Amount:  -arg.Amount,
+            })
+            if err != nil {
+                return err
+            }
+
+       }
+       
 
         return nil
     })
