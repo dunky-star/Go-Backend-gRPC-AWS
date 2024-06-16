c5a6c24fdced6aec58b63fd38a33ae33a914e38f
dunky-star <dunkygeoffrey39@gmail.com>
2024-06-16 16:28:53 -0500

    Rest API - HTTP JSON

--- api/server.go
+++ api/server.go
@@ -1,7 +1,28 @@
 package api
 
-// The server will server all API for our banking service
-type Server struct{
-	
+import (
+	db "github.com/dunky-star/u_bank/sqlc"
+	"github.com/gin-gonic/gin"
+)
 
+// Server serves HTTP requests for our banking service.
+type Server struct {
+    store  *db.Store
+    router *gin.Engine
 }
+
+// NewServer creates a new HTTP server and setup routing.
+func NewServer(store *db.Store) *Server {
+    server := &Server{store: store}
+    router := gin.Default()
+
+    router.POST("/accounts", server.createAccount)
+
+    server.router = router
+    return server
+}
+
+func errorResponse(err error) gin.H {
+    return gin.H{"error": err.Error()}
+}
+
