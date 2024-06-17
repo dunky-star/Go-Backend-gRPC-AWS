6cec8e06be5b4cef15ff3aca7db7af4604db22b4
dunky-star <dunkygeoffrey39@gmail.com>
2024-06-16 21:13:05 -0500

    Rest API - HTTP JSON

--- api/server.go
+++ api/server.go
@@ -16,10 +16,15 @@ func NewServer(store *db.Store) *Server {
     server := &Server{store: store}
     router := gin.Default()
 
-    router.POST("/accounts", server.createAccount)
+    router.POST("/api/v1/accounts", server.createAccount)
 
     server.router = router
     return server
+}
+
+// Start runs the Http server on a specific address
+func (server *Server) Start(address string) error{
+	return server.router.Run(address)
 }
 
 func errorResponse(err error) gin.H {
--- main.go
+++ main.go
@@ -1,23 +1,31 @@
 package main
 
-// import (
-// 	"database/sql"
-// 	"log"
+import (
+	"database/sql"
+	"log"
 
-// 	_ "github.com/lib/pq"
-// )
+	"github.com/dunky-star/u_bank/api"
+	db "github.com/dunky-star/u_bank/sqlc"
+	_ "github.com/lib/pq"
+)
 
-// const (
-// 	dbDriver      = "postgres"
-// 	dbSource      = "postgresql://postgres:cluster@1@localhost:5432/u_bank?sslmode=disable"
-// 	serverAddress = "0.0.0.:9090"
-// )
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://postgres:cluster@1@localhost:5432/u_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:9090"
+)
 
-// func main() {
-// 	conn, err := sql.Open(dbDriver, dbSource)
-// 	if err != nil{
-// 		log.Fatal("Cannot connect to DB: ", err)
-// 	}
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil{
+		log.Fatal("Cannot connect to DB: ", err)
+	}
 
-// 	store := db.NewStore(conn)
-// }
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil{
+		log.Fatal("Cannot start server ", err)
+	}
+}
