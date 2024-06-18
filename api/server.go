9cf72201860ffa299599687ecf11af6eac561302
dunky-star <dunkygeoffrey39@gmail.com>
2024-06-17 22:01:43 -0500

    HPPT REST API - Go

--- api/server.go
+++ api/server.go
@@ -7,18 +7,19 @@ import (
 
 // Server serves HTTP requests for our banking service.
 type Server struct {
-    store  *db.Store
+    store  db.Store
     router *gin.Engine
 }
 
 // NewServer creates a new HTTP server and setup routing.
-func NewServer(store *db.Store) *Server {
+func NewServer(store db.Store) *Server {
     server := &Server{store: store}
     router := gin.Default()
 
     router.POST("/api/v1/accounts", server.createAccount)
 	router.GET("/api/v1/accounts/:id", server.getAccount)
 	router.GET("/api/v1/accounts", server.listAccount)
+    router.POST("/api/v1/transfers", server.createTransfer)
 
 
     server.router = router
