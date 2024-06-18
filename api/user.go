ebbd2660777cdd7f686707d8a8dc0b426b3b9321
dunky-star <dunkygeoffrey39@gmail.com>
2024-06-18 16:42:46 -0500

    Paseto auth token

--- api/user.go
+++ api/user.go
@@ -2,7 +2,7 @@ package api
 
 import (
 	"net/http"
-
+    "time"
 	db "github.com/dunky-star/u_bank/sqlc"
 	"github.com/dunky-star/u_bank/util"
 	"github.com/gin-gonic/gin"
@@ -14,6 +14,14 @@ type createUserRequest struct {
 	Password string `json:"password" binding:"required,min=6"`
 	FullName string `json:"full_name" binding:"required"`
 	Email    string `json:"email" binding:"required,email"`
+}
+
+type createUserResponse struct {
+	Username          string    `json:"username"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
 }
 
 func (server *Server) createUser(ctx *gin.Context) {
@@ -48,6 +56,15 @@ func (server *Server) createUser(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
-	ctx.JSON(http.StatusOK, user)
+
+	rsp := createUserResponse{
+		Username:         user.Username,
+		FullName:         user.FullName,
+		Email:            user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt:        user.CreatedAt,
+	}
+	ctx.JSON(http.StatusOK, rsp)
+	
 
 }
